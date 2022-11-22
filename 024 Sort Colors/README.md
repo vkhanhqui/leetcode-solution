aa5d8a1de404010b4530a381b4ace5b8f6149638
Steve <vkhanhqui@gmail.com>
2022-11-22 12:40:07 +0700

    Solved Sort Colors problem

---
+++ 024 Sort Colors/README.md
@@ -0,0 +1,5 @@
+# 75. Sort Colors
+
+```
+https://leetcode.com/problems/sort-colors
+```
---
+++ 024 Sort Colors/solution1.py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        new_dict = defaultdict(list)
+        for num in nums:
+            new_dict[num].append(num)
+        prev_slice = 0
+        for i in range(0, 3, 1):
+            nums[prev_slice:len(new_dict[i])+prev_slice] = new_dict[i]
+            prev_slice += len(new_dict[i])
